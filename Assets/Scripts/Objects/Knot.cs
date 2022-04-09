d50e774940cde5cdb4779da1dd23b51ecea84515
htdat2000 <12a6.ht.dat@gmail.com>
2022-04-09 19:44:38 +0700

    GridManager
    a 2 dimensional array grid to hold all knots

---
+++ Assets/Scripts/Objects/Knot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knot : MonoBehaviour
+{
+    private int xIndex;
+    private int yIndex;
+    private bool isWalkable = true;
+
+    public void SetKnotIndex(int x, int y)
+    {
+        xIndex = x;
+        yIndex = y;
+    }
+}
