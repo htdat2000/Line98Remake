496313d6f814afd5a0533d7383ceec1472b04459
htdat2000 <12a6.ht.dat@gmail.com>
2022-04-09 18:33:03 +0700

    SpawnKnotByCode

---
+++ Assets/Scripts/GameController/KnotGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnotGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject knot;
+    [SerializeField] GameObject knotHolder;
+    public static KnotGenerator instance;
+    protected void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than 1 KnotGenerator in scene");
+            return;
+        }
+        instance = this;
+        InstantiateKnot(9,9);
+    }
+
+    public async void InstantiateKnot(int row, int column)
+    {
+        DebugMissingPrefab();
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                Vector2 pos = new Vector2 (i, j);
+                GameObject newKnot = Instantiate(knot, pos, Quaternion.identity);
+                newKnot.transform.SetParent(knotHolder.transform);
+            }
+        }
+    }
+
+    void DebugMissingPrefab()
+    {
+        if(knot == null)
+        {
+            Debug.LogError("No knot prefab");
+        }
+        if(knotHolder == null)
+        {
+            Debug.LogError("No knot holder");
+        }
+    }
+}
